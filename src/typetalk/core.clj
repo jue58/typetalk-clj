030b83bc9aa12a26d80d5ea19a636fe6de119b80
Takuji Shimokawa <shimokawa@soba-project.com>
2014-03-05 15:52:08 +0900

    Renames functions and adds placeholders

--- src/typetalk/core.clj
+++ src/typetalk/core.clj
@@ -2,7 +2,7 @@
   (:require [clj-http.client :as http]
             [clojure.data.json :as json]))
 
-(defn access_token [client_id client_secret scope]
+(defn get-access-token [client_id client_secret scope]
   (let [res (http/post
               "https://typetalk.in/oauth2/access_token"
               {:form-params
@@ -15,7 +15,7 @@
     (if (= status 200)
       (json/read-str body))))
 
-(defn profile [access_token]
+(defn get-profile [access_token]
   "Fetch the user profile of the user who got the given access token"
   (let [res (http/get
               "https://typetalk.in/api/v1/profile"
@@ -27,7 +27,7 @@
   (let [arr (json "topics")]
     (map #(% "topic") arr)))
 
-(defn topics [access_token]
+(defn get-topics [access_token]
   "Fetch topics"
   (let [res (http/get
               "https://typetalk.in/api/v1/topics"
@@ -36,14 +36,14 @@
       (-> (json/read-str (:body res))
         (topics-response->topics)))))
 
-(defn posts [access_token topic]
+(defn get-posts [access_token topic]
   (let [res (http/get
               (str "https://typetalk.in/api/v1/topics/" (topic "id"))
               {:query-params {:access_token access_token}})]
     (if (= 200 (:status res))
       ((json/read-str (:body res)) "posts"))))
 
-(defn new-post [access_token topic message]
+(defn create-post [access_token topic message]
   (let [res (http/post
               (str "https://typetalk.in/api/v1/topics/" (topic "id"))
               {:form-params
@@ -51,9 +51,43 @@
     (if (= (:status res) 200)
       (json/read-str (:body res)))))
 
-(defn post [access_token topic post]
+(defn get-post [access_token topic post]
   (let [res (http/get
               (str "https://typetalk.in/api/v1/topics/" (topic "id") "/posts/" (post "id"))
               {:query-params {:access_token access_token}})]
     (if (= 200 (:status res))
       (json/read-str (:body res)))))
+
+(defn delete-post [access_id topic post]
+  (println "not implemented"))
+
+(defn create-like [access_id topic post]
+  (println "not implemented"))
+
+(defn delete-like [access_id topic post]
+  (println "not implemented"))
+
+(defn create-favorite [access_id topic]
+  (println "not implemented"))
+
+(defn delete-favorite [access_id topic]
+  (println "not implemented"))
+
+(defn get-notifications [access_id]
+  (println "not implemented"))
+
+(defn open-notifications [access_id]
+  (println "not implemented"))
+
+(defn mark-topic-as-read
+  [access_id topic]
+  (println "not implemented")
+  [access_id topic post]
+  (println "not implemented"))
+
+(defn get-mentions [access_id &options]
+  (println "not implemented"))
+
+(defn mark-mention-as-read [access_id mention]
+  (println "not implemented"))
+
