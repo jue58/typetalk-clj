a82b24e390971bda77b163adc4f12ad4e3b2a004
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2014-03-06 10:19:12 +0900

    Adds environ.

--- test/typetalk/core_test.clj
+++ test/typetalk/core_test.clj
@@ -1,7 +1,19 @@
 (ns typetalk.core-test
   (:require [clojure.test :refer :all]
-            [typetalk.core :refer :all]))
+            [typetalk.core :refer :all])
+  (:use [environ.core]))
+
+(def typetalk-creds
+  {:client_id (env :typetalk-client-id)
+   :client_secret (env :typetalk-client-secret)})
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "get-access-token gets an access token."
+    (println (:client_id typetalk-creds))    
+    (println (:client_secret typetalk-creds))
+    (let [token (get-access-token (:client_id typetalk-creds)
+                                  (:client_secret typetalk-creds)
+                                  "topic.read,topic.post,my")]
+      (is (not (nil? (token "access_token")))))))
+
+
