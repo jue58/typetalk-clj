41ac1626c5b0c72d1936af78e0dd502de4bab793
Takuji Shimokawa <shimokawa@soba-project.com>
2014-03-06 16:25:24 +0900

    Adds a test for get-profile

--- test/typetalk/core_test.clj
+++ test/typetalk/core_test.clj
@@ -13,3 +13,16 @@
                                   (:client_secret typetalk-creds)
                                   "topic.read,topic.post,my")]
       (is (not (nil? (token "access_token")))))))
+
+(deftest test-get-profile
+  (let [token (get-access-token (:client_id typetalk-creds)
+                                (:client_secret typetalk-creds)
+                                "my")
+        access_token (token "access_token")]
+    (testing "profile has name and some other properties"
+      (let [profile (get-profile access_token)]
+        (is (not (nil? (profile "account"))))
+        (is (not (nil? (get-in profile ["account" "name"]))))
+        (is (not (nil? (get-in profile ["account" "fullName"]))))
+        (is (not (nil? (get-in profile ["account" "imageUrl"]))))
+        ))))
