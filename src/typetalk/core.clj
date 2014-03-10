a78c29d6b50fb8fc1b722eeb4561ad20a48e0400
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2014-03-10 23:52:03 +0900

    Extracts HTTP requests to shared functions.

--- src/typetalk/core.clj
+++ src/typetalk/core.clj
@@ -3,6 +3,44 @@
             [clj-http.util :as http-util]
             [clojure.data.json :as json]))
 
+(defn- get-request [access-token path params]
+  (http/get
+    (str "https://typetalk.in/api/v1/" path)
+    (merge {:headers {"Authorization" (str "Bearer " access-token)}}
+           params)))
+
+(defn- post-request [access-token path params]
+  (http/post
+    (str "https://typetalk.in/api/v1/" path)
+    (merge {:headers {"Authorization" (str "Bearer " access-token)}
+            :form-params params})))
+
+(defn- get-api [access-token path & params]
+  (let [res (get-request access-token path (apply hash-map params))]
+    (if (= 200 (:status res))
+      (json/read-str (:body res)))))
+
+(defn- post-api [access-token path params]
+  (let [res (post-request access-token path params)]
+    (if (= (:status res) 200)
+      (json/read-str (:body res)))))
+
+(defn delete-api [access-token path]
+  (let [res (http/delete
+              (str "https://typetalk.in/api/v1/" path)
+              {:headers {"Authorization" (str "Bearer " access-token)}})]
+    (if (= (:status res) 200)
+      (json/read-str (:body res)))))
+
+(defn put-api [access-token path]
+  "scope: my"
+  (let [res (http/put
+              (str "https://typetalk.in/api/v1/" path)
+              {:headers {"Authorization" (str "Bearer " access-token)}})]
+    (if (= 200 (:status res))
+      (json/read-str (:body res)))))
+
+
 (defn get-access-token [client_id client_secret scope]
   (let [res (http/post
               "https://typetalk.in/oauth2/access_token"
@@ -30,132 +68,80 @@
     (if (= 200 (:status res))
       (json/read-str (:body res)))))
 
+
 (defn get-profile [access_token]
   "Fetch the user profile of the user who got the given access token"
-  (let [res (http/get
-              "https://typetalk.in/api/v1/profile"
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (get-api access_token "profile"))
 
 (defn get-topics [access_token]
   "Fetch topics"
-  (let [res (http/get
-              "https://typetalk.in/api/v1/topics"
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (get-api access_token "topics"))
 
 (defn get-posts [access_token topic]
-  (let [res (http/get
-              (str "https://typetalk.in/api/v1/topics/" (topic "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)) "posts")))
+  (get-api access_token
+           (str "topics/" (topic "id"))))
 
-(defn create-post [access_token topic message]
-  (let [res (http/post
-              (str "https://typetalk.in/api/v1/topics/" (topic "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}
-               :form-params {:message message}})]
-    (if (= (:status res) 200)
-      (json/read-str (:body res)))))
+(defn create-post [access-token topic message]
+  (post-api access-token
+            (str "topics/" (topic "id"))
+            {:message message}))
 
 (defn get-post [access_token post]
-  (let [res (http/get
-              (str "https://typetalk.in/api/v1/topics/" (post "topicId") "/posts/" (post "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (get-api access_token
+           (str "topics/" (post "topicId") "/posts/" (post "id"))))
 
-(defn delete-post [access_token post]
-  (let [res (http/delete
-              (str "https://typetalk.in/api/v1/topics/" (post "topicId") "/posts/" (post "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+(defn delete-post [access-token post]
+  (delete-api access-token
+              (str "topics/" (post "topicId") "/posts/" (post "id"))))
 
-(defn create-like [access_token post]
+(defn create-like [access-token post]
   "Adds LIKE to a post
    scope: topic.post"
-  (let [res (http/post
-              (format "https://typetalk.in/api/v1/topics/%s/posts/%s/like" (post "topicId") (post "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= (:status res) 200)
-      (json/read-str (:body res)))))
+  (post-api access-token
+            (format "topics/%s/posts/%s/like" (post "topicId") (post "id"))
+            {}))
 
-(defn delete-like [access_token post]
+(defn delete-like [access-token post]
   "Deletes LIKE to a post
    scope: topic.post"
-  (let [res (http/delete
-              (format "https://typetalk.in/api/v1/topics/%s/posts/%s/like" (post "topicId") (post "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= (:status res) 200)
-      (json/read-str (:body res)))))
+  (delete-api access-token
+              (format "topics/%s/posts/%s/like" (post "topicId") (post "id"))))
 
-(defn create-favorite [access_token topic]
+(defn create-favorite [access-token topic]
   "scope: my"
-  (let [res (http/post
-              (format "https://typetalk.in/api/v1/topics/%s/favorite" (topic "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= (:status res) 200)
-      (json/read-str (:body res)))))
+  (post-api access-token
+            (format "topics/%s/favorite" (topic "id"))
+            {}))
 
-(defn delete-favorite [access_token topic]
+(defn delete-favorite [access-token topic]
   "scope: my"
-  (let [res (http/delete
-              (format "https://typetalk.in/api/v1/topics/%s/favorite" (topic "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= (:status res) 200)
-      (json/read-str (:body res)))))
+  (delete-api access-token
+              (format "topics/%s/favorite" (topic "id"))))
 
 (defn get-notifications [access_token]
   "scope: my"
-  (let [res (http/get
-              (str "https://typetalk.in/api/v1/notifications/status")
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (get-api access_token "notifications/status"))
 
-(defn open-notifications [access_token topic]
+(defn open-notifications [access-token topic]
   "scope: my"
-  (let [res (http/put
-              (str "https://typetalk.in/api/v1/notifications/open")
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (put-api access-token "notifications/open"))
 
-(defn mark-topic-as-read [access_token topic]
+(defn mark-topic-as-read [access-token topic]
   "scope: my"
-  (let [res (http/post
-              (str "https://typetalk.in/api/v1/bookmark/save")
-              {:headers {"Authorization" (str "Bearer " access_token)}
-               :form-params {:topicId (topic "id")}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (post-api access-token
+            "bookmark/save"
+            {:topicId (topic "id")}))
 
-(defn mark-post-as-read [access_token post]
+(defn mark-post-as-read [access-token post]
   "scope: my"
-  (let [res (http/post
-              (str "https://typetalk.in/api/v1/bookmark/save")
-              {:headers {"Authorization" (str "Bearer " access_token)}
-               :form-params {:topicId (post "topicId") :postId (post "id")}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (post-api access-token
+            "bookmark/save"
+            {:topicId (post "topicId") :postId (post "id")}))
 
-(defn get-mentions [access_token & options]
+(defn get-mentions [access-token & options]
   "scope: my"
-  (let [res (http/get
-              (str "https://typetalk.in/api/v1/mentions")
-              {:headers {"Authorization" (str "Bearer " access_token)}
-               :query-params (apply hash-map options)})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (get-api access-token "mentions" :query-params (apply hash-map options)))
 
 (defn mark-mention-as-read [access_token mention]
   "scope: my"
-  (let [res (http/put
-              (str "https://typetalk.in/api/v1/mentions/" (mention "id"))
-              {:headers {"Authorization" (str "Bearer " access_token)}})]
-    (if (= 200 (:status res))
-      (json/read-str (:body res)))))
+  (put-api (str "mentions/" (mention "id"))))
